17e81f951d3b5b74a00886cf7a5d0121d2c94a3e
Wirgiliusz <kamx98@gmail.com>
2019-06-05 22:23:34 +0200

    Create siec.py

---
+++ siec.py
@@ -0,0 +1,7 @@
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+
