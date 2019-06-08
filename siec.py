cbd34bff9fb7ea8b2192933ede49000e751a4c2c
Wirgiliusz <kamx98@gmail.com>
2019-06-08 13:28:40 +0200

    ogarniecie subplotow

--- siec.py
+++ siec.py
@@ -1,17 +1,17 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+from sklearn.linear_model import Perceptron
 from sklearn.neural_network import MLPClassifier  
+from sklearn.neighbors import KNeighborsClassifier
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 iris = datasets.load_iris()
-
 X = iris['data']
 y = iris['target']
 
@@ -38,6 +38,12 @@ for i in range(0, 150):
     elif(y[i] == 2):
         kolor.append('bo')
 
+
+fig1 = plt.figure(1)
+sub1 = fig1.add_subplot(221)
+sub2 = fig1.add_subplot(222)
+sub3 = fig1.add_subplot(223)
+sub4 = fig1.add_subplot(224)
 for i in range(0, 150):
     if(i==0):
         n = nazwa[0]
@@ -48,41 +54,34 @@ for i in range(0, 150):
     else:
         n = nazwa[3]
 
-    plt.subplot(221)
-    plt.plot(s_l[i], p_l[i], color=kolor[i][0], marker=kolor[i][1], label=n)
-    plt.subplot(222)
-    plt.plot(s_w[i], p_w[i], color=kolor[i][0], marker=kolor[i][1], label=n)
-    plt.subplot(223)
-    plt.plot(s_l[i], s_w[i], color=kolor[i][0], marker=kolor[i][1], label=n)
-    plt.subplot(224)
-    plt.plot(p_w[i], p_l[i], color=kolor[i][0], marker=kolor[i][1], label=n)
+    sub1.plot(s_l[i], p_l[i], color=kolor[i][0], marker=kolor[i][1], label=n)
+    sub2.plot(s_w[i], p_w[i], color=kolor[i][0], marker=kolor[i][1], label=n)
+    sub3.plot(s_l[i], s_w[i], color=kolor[i][0], marker=kolor[i][1], label=n)
+    sub4.plot(p_w[i], p_l[i], color=kolor[i][0], marker=kolor[i][1], label=n)
 
-plt.subplot(221)
-plt.title("Petal(Sepal) length")
-plt.xlabel('Sepal length [cm]')
-plt.ylabel('Petal length [cm]')
-plt.legend()
 
-plt.subplot(222)
-plt.title("Petal(Sepal) width")
-plt.xlabel('Sepal width [cm]')
-plt.ylabel('Petal width [cm]')
-plt.legend()
+sub1.set_title("Petal(Sepal) length")
+sub1.set_xlabel('Sepal length [cm]')
+sub1.set_ylabel('Petal length [cm]')
+sub1.legend()
 
-plt.subplot(223)
-plt.title("Sepal_len(Sepal_wid)")
-plt.xlabel('Sepal width [cm]')
-plt.ylabel('Sepal length [cm]')
-plt.legend()
+sub2.set_title("Petal(Sepal) width")
+sub2.set_xlabel('Sepal width [cm]')
+sub2.set_ylabel('Petal width [cm]')
+sub2.legend()
 
-plt.subplot(224)
-plt.title("Petal_len(Petal_wid)")
-plt.xlabel('Petal width [cm]')
-plt.ylabel('Petal length [cm]')
-plt.legend()
+sub3.set_title("Sepal_len(Sepal_wid)")
+sub3.set_xlabel('Sepal width [cm]')
+sub3.set_ylabel('Sepal length [cm]')
+sub3.legend()
 
-#plt.show()
+sub4.set_title("Petal_len(Petal_wid)")
+sub4.set_xlabel('Petal width [cm]')
+sub4.set_ylabel('Petal length [cm]')
+sub4.legend()
 
+plt.show()
+# - - - - - - - - - - - - - - - #
 
 # Podzial danych na grupe uczenia i testowania
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -93,16 +92,21 @@ sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
 
+
 # - Perceptron - #
 ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
 ppn.fit(X_train_std, y_train)
 y_pred_ppn = ppn.predict(X_test_std)
 
-
 # - Wielowarstwowy perceptron - #
 mlp = MLPClassifier(hidden_layer_sizes=(10, 10), activation="relu", solver='adam', max_iter=1000)
 mlp.fit(X_train, y_train)  
 y_pred_mlp = mlp.predict(X_test)
+
+# - K najblizszych sasiadow - #
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+y_pred_knn = knn.predict(X_test)
 
 print("Rzeczywiste wyniki:")
 print(y_test)
@@ -110,6 +114,10 @@ print("Przewidywania peceptrona:")
 print(y_pred_ppn)
 print("Przewidywania wielowarstwowego perceptrona:")
 print(y_pred_mlp)
+print("Przewidywania K najblizszych sasiadow:")
+print(y_pred_knn)
 
 print('Dokladnosc perceptrona: %.2f' % (accuracy_score(y_test, y_pred_ppn)*100), "%")
 print('Dokladnosc wielowarstwowego perceptrona: %.2f' % (accuracy_score(y_test, y_pred_mlp)*100), "%")
+print('Dokladnosc K najblizszych sasiadow: %.2f' % (accuracy_score(y_test, y_pred_knn)*100), "%")
+
