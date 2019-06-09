fd6e2f62e7ee82f5af69a8e023319385440ea9fb
Wirgiliusz <kamx98@gmail.com>
2019-06-09 03:06:48 +0200

    wykresy dla MLP

--- global.py
+++ global.py
@@ -64,13 +64,13 @@ sub30.scatter(Xs[:,0], Xs[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
 fig1.suptitle('Sepal Width(Length) and Petal Width(Length)\n for KNeighbors Classifier')
 sub1.set_xlabel('Length [cm]')
 sub1.set_ylabel('Width [cm]')
-sub1.set_title('\nK=1')
+sub1.set_title('K=1')
 sub10.set_xlabel('Length [cm]')
 sub10.set_ylabel('Width [cm]')
 sub10.set_title('[Sepal] K=10')
 sub30.set_xlabel('Length [cm]')
 sub30.set_ylabel('Width [cm]')
-sub30.set_title('\nK=30')
+sub30.set_title('K=30')
 
 # dla Petal #
 Xp = X[:,2:4] # petal length/width
@@ -119,5 +119,105 @@ sub10.set_title('[Petal] K=10')
 sub30.set_xlabel('Length [cm]')
 sub30.set_ylabel('Width [cm]')
 sub30.set_title('K=30')
+
+
+# - - - - - MLP - - - - - #
+# dla Sepal #
+Xs = X[:,0:2] # sepal length/width
+mlp1 = MLPClassifier(hidden_layer_sizes=(10,), activation="relu", solver='adam', max_iter=1000)
+mlp3 = MLPClassifier(hidden_layer_sizes=(10,10,10,), activation="relu", solver='adam', max_iter=1000)
+mlp5 = MLPClassifier(hidden_layer_sizes=(10,10,10,10,10,), activation="relu", solver='adam', max_iter=1000)
+mlp1.fit(Xs, y)
+mlp3.fit(Xs, y)
+mlp5.fit(Xs, y)
+
+x_min, x_max = Xs[:,0].min()-.1, Xs[:,0].max()+.1
+y_min, y_max = Xs[:,1].min()-.1, Xs[:,1].max()+.1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+# Przewidywanie
+Z1 = mlp1.predict(np.c_[xx.ravel(), yy.ravel()])    
+Z10 = mlp3.predict(np.c_[xx.ravel(), yy.ravel()])
+Z30 = mlp5.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z1 = Z1.reshape(xx.shape)
+Z10 = Z10.reshape(xx.shape)
+Z30 = Z30.reshape(xx.shape)
+
+# tworzenie plotow
+fig2 = plt.figure(2)
+sub1 = fig2.add_subplot(231)
+sub3 = fig2.add_subplot(232)
+sub5 = fig2.add_subplot(233)
+
+# rysowanie granic
+sub1.pcolormesh(xx, yy, Z1, cmap=cmap_light)
+sub3.pcolormesh(xx, yy, Z10, cmap=cmap_light)
+sub5.pcolormesh(xx, yy, Z30, cmap=cmap_light)
+
+# rysowanie punktow
+sub1.scatter(Xs[:,0], Xs[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+sub3.scatter(Xs[:,0], Xs[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+sub5.scatter(Xs[:,0], Xs[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+
+# nazywanie wykresow
+fig2.suptitle('Sepal Width(Length) and Petal Width(Length)\n for M-L Perceptron')
+sub1.set_xlabel('Length [cm]')
+sub1.set_ylabel('Width [cm]')
+sub1.set_title('N Layers = 1')
+sub3.set_xlabel('Length [cm]')
+sub3.set_ylabel('Width [cm]')
+sub3.set_title('[Sepal] N Layers = 3')
+sub5.set_xlabel('Length [cm]')
+sub5.set_ylabel('Width [cm]')
+sub5.set_title('N Layers = 5')
+
+# dla Petal #
+Xp = X[:,2:4] # petal length/width
+mlp1 = MLPClassifier(hidden_layer_sizes=(10,), activation="relu", solver='adam', max_iter=1000)
+mlp3 = MLPClassifier(hidden_layer_sizes=(10,10,10,), activation="relu", solver='adam', max_iter=1000)
+mlp5 = MLPClassifier(hidden_layer_sizes=(10,10,10,10,10,), activation="relu", solver='adam', max_iter=1000)
+mlp1.fit(Xp, y)
+mlp3.fit(Xp, y)
+mlp5.fit(Xp, y)
+
+x_min, x_max = Xp[:,0].min()-.3, Xp[:,0].max()+.3
+y_min, y_max = Xp[:,1].min()-.3, Xp[:,1].max()+.3
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+# Przewidywanie
+Z1 = mlp1.predict(np.c_[xx.ravel(), yy.ravel()])    
+Z10 = mlp3.predict(np.c_[xx.ravel(), yy.ravel()])
+Z30 = mlp5.predict(np.c_[xx.ravel(), yy.ravel()])
+
+Z1 = Z1.reshape(xx.shape)
+Z10 = Z10.reshape(xx.shape)
+Z30 = Z30.reshape(xx.shape)
+
+# tworzenie plotow
+sub1 = fig2.add_subplot(234)
+sub3 = fig2.add_subplot(235)
+sub5 = fig2.add_subplot(236)
+
+# rysowanie granic
+sub1.pcolormesh(xx, yy, Z1, cmap=cmap_light)
+sub3.pcolormesh(xx, yy, Z10, cmap=cmap_light)
+sub5.pcolormesh(xx, yy, Z30, cmap=cmap_light)
+
+# rysowanie punktow
+sub1.scatter(Xp[:,0], Xp[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+sub3.scatter(Xp[:,0], Xp[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+sub5.scatter(Xp[:,0], Xp[:,1], c=y, cmap=cmap_bold, edgecolor='k', s=20)
+
+# nazywanie wykresow
+sub1.set_xlabel('Length [cm]')
+sub1.set_ylabel('Width [cm]')
+sub1.set_title('N Layers = 1')
+sub3.set_xlabel('Length [cm]')
+sub3.set_ylabel('Width [cm]')
+sub3.set_title('[Sepal] N Layers = 3')
+sub5.set_xlabel('Length [cm]')
+sub5.set_ylabel('Width [cm]')
+sub5.set_title('N Layers = 5')
 
 plt.show()
